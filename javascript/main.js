e7cfe2742700630190e7c77e84154a0d518dfebe
Jason Gerecke <killertofu@gmail.com>
2012-10-07 15:05:59 -0700

    Add Apache license

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2012 Jason Gerecke
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 var gamejs = require('gamejs');
 var WIDTH  = 1280
 var HEIGHT = 960
@@ -466,7 +482,7 @@ function Outro(shell, time) {
 		var textsurf = textfont.render(seconds + "." + millis + " seconds");
 		size = textsurf.getSize();
 		size[0] = getX(0.5) - size[0]/2;
-		size[1] = getY(0.5) - size[1]/2;
+		size[1] = getY(0.4) - size[1]/2;
 		surface.blit(textsurf, size);
 
 		textsurf = textfont.render("Press <ENTER> to Play Again");
